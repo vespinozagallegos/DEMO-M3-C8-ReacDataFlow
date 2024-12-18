f7d10610640da125984c163b402a3bf38e6bf93d
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-18 17:28:11 -0300

    se edita el modulo pero aun no funciona el contador

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import CardList from './CardList';
 
 
 function App() {
+  let counter = 0;
+  console.log(counter)
+
   return (
     <>
       <h1>Mi Primera App</h1>
       <CardList/>
+
+      <p>Clicks: {counter}</p>
+      <button onClick={() => counter++}>Contador de Clicks</button>
     </>
   );
 };
