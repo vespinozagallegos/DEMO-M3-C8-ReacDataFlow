c4ac5a35b7bc0a71f6f0edb88f28bbb40b2e74f5
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-19 16:41:12 -0300

    Se agregó una función básica para mostrar Register o Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
 import './App.css'
+import Login from './Login';
+import Register from './Register';
 
 function App() {
+  const isRegistered = true;
+
+  if (isRegistered) {
+    return (
+
+      <div>
+
+        <h1>Bienvenidos a mi Primera App</h1>
+        <h2>Ingresa tus datos para entrar.</h2>
+        
+        <Login/>
+
+      </div>
+
+    )    
+  }
+    return (
+      <div>
+        <h1>Bienvenidos a mi Primera App</h1>
+        <h2>Completa los siguientes datos.</h2>
+
+        <Register/>
+
+      </div>
+
+
+    )
+  
+
+  // return (
+  //   <>
+  //   <Login/>
+  //   <Register/>
+  //   </>
+  // );
 };
 
 export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,20 @@
+const Login = () => {
+    return (
+        <div>
+            <h2>Login</h2>
+
+            <form>
+                <label>Email: </label>
+                <input type="text" />
+
+                <label>Contraseña: </label>
+                <input type="text" />
+            </form>
+
+
+        </div>
+
+    );
+}
+
+export default Login;
