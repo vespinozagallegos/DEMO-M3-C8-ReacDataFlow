0b849b462b4a2b7dade536a4c368f4b38db290ce
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-19 16:58:29 -0300

    Se implementó el operador ternario para mostrar Register o Login

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function App() {
 
         <h1>Bienvenidos a mi Primera App</h1>
         
-        {isRegistered && <> <h2>Ingresa tus datos para entrar.</h2><Login/> </>}
-        {!isRegistered && <> <h2>Completa los siguientes datos.</h2><Register/> </>}
+        {isRegistered ? <> <h2>Ingresa tus datos para entrar.</h2><Login/> </>  :   <> <h2>Completa los siguientes datos.</h2><Register/> </>}
 
       </>
 
