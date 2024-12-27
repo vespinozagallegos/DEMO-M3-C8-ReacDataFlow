d6193886db6d167ede2efe284518aa48be4b2bc1
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-27 18:36:54 -0300

    Se dejó comentado el archivo index.css para implementar esta segunda forma de dar estilos module css. Se creo el modulo App.module.css y se importo en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import CardList from './CardList';
+import styles from './App.module.css' //Se importa el modulo completo
+console.log(styles);
+
 
 function App() {
   const counter = 0;
 
   return (
     <>
-      <h1 className='title'>Mi Primera App</h1>
-      <h2>Contador: {counter}</h2>
+      <h1 className={styles.title}>Mi Primera App</h1>
+      <h2 className={styles.subtitle}>Contador: {counter}</h2>
       <button>Contador de clicks</button>
       <CardList/>
 {/* 
