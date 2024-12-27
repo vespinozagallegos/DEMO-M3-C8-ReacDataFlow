d86cd01473cd359c08bdd3276d2bb11983ef2d8b
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-27 20:01:35 -0300

    Se creó Card.module.css. Se definieron los estilos. Y se importaron en Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,9 @@
+import styles from './Card.module.css'
+
 const Card = () => {
     return (
         //Aqui debe ir un div, tenia fragment pero se desordenaba el contenido de la tarjeta al duplicarla en CardList
-        <div>
+        <div className={styles.container}>
             <h1>Tarjeta</h1>
             <h3>Nombre: Víctor</h3>
             <p>Email: victor@mail.cl</p>
