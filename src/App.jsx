53c51d80f7c62628d1d3e029e9e399710daf9b30
vespinozagallegos <espinozagallegosvictor@gmail.com>
2024-12-30 15:52:30 -0300

    Se pusieron comentarios a la importacion del HOOK useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import CardList from './CardList';
 import styles from './App.module.css' //Se importa el modulo completo
 import { CustomButton } from './CustomButton';  // Se importa el Componente estilizado (Styled-Component)
 // console.log(CustomButton); //es un objeto
-// import { useState } from 'react';
+import { useState } from 'react';   // HOOK de React. Desectructurado porque esta dentro de la dependencia de React que contiene muchas cosas
 
 function App() {
   //esta es la logica
+  //CREACIÓN DE ESTADO.
+  // Se desectructura el array. count es el valor del estado que va a variar.
+  // set+estado(por convención)=setCount. //*Es la función que permite modificar el estado "count"
+  // Luego se llama al hook useState. //*Y se le da un valor inicial (0) que será el valor inicial del estado "count".
   const [count, setCount] = useState(0)
   //el return es un componente, mezcla js con html (etiquetas, que pueden tener o no lógica)
   return (
