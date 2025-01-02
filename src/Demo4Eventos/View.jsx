3ebb605edcc9990697186f09818c9ffd20e3ac66
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-02 17:15:00 -0300

    Se crea una funcion, con parametro info, para ver como se traspasa al hijo. Se agrega como props en el return de View y ya se puede usar en User

--- src/Demo4Eventos/View.jsx
+++ src/Demo4Eventos/View.jsx
@@ -1,8 +1,16 @@
 import User from "./User";
 
 const View = () => {
+    //funcion para ver como funciona el Evento de Padre a Hijo
+    //info es un parametro por estar en una función normal, NO ES PROPS. Sería props si estuviera arriba en el comp. View = ()
+    const handleUserButtonClick = (info) => {
+        console.log(info);
+    }
+    //Para ver si llega la info debo pasarle handleUserButtonClick como PROP al hijo User
+    //La prop lleva el mismo nombre de la funcion handleUserButtonClick
+    //*Con esto le pasamos una Prop al Hijo User que irá autom. en el paréntesis -> const User = () y se puede desestructurar en el modulo User
     return (
-        <User/>
+        <User handleUserButtonClick={handleUserButtonClick}/>
     )
 }
 
