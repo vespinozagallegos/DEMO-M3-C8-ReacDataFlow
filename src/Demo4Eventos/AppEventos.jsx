a38df104672a47efdcf4855d1780f8ae13ebc5d2
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-02 17:33:41 -0300

    Se renderiza el Componentente PADRE View

--- src/Demo4Eventos/AppEventos.jsx
+++ src/Demo4Eventos/AppEventos.jsx
@@ -1,11 +1,12 @@
 import '../App.css'
 import styles from '../App.module.css'
+import View from './View';
 console.log(styles);
 
 function AppEventos () {
     return (
         <>
-
+            <View/>
         </>
     )
 }
