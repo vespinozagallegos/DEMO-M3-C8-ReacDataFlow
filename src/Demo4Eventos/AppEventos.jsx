5709551b3ca37afb792966e33371707456534b44
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-02 20:35:28 -0300

    FIN DE LA DEMO 4, se renderizan juntos y podrian estar en pantallas separadas y aun asi compartir la accion

--- src/Demo4Eventos/AppEventos.jsx
+++ src/Demo4Eventos/AppEventos.jsx
@@ -1,12 +1,14 @@
 import '../App.css'
 import styles from '../App.module.css'
-import View from './View';
+// import View from './View';
+import ParentComponent from './ParentComponent';
 console.log(styles);
 
 function AppEventos () {
     return (
         <>
-            <View/>
+            {/* <View/> */}
+            <ParentComponent/>
         </>
     )
 }
